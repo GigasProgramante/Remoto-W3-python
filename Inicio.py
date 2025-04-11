984032d9bba855225cfdbbe1f15df659a1cd3d7a
Guigas <guilhermevoj@gmail.com>
2025-04-11 13:07:40 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -1,1 +1,4 @@
-algo
+# Em python, não existe um comando especifico para definir uma variavel, invés disso, podemos simplesmente atribuir um valor a uma variavel.
+x = 10
+palavra = "Olá Mundo!"
+# O python é uma linguagem de tipagem dinâmica, ou seja, não precisamos declarar o tipo da variavel, o python infere o tipo da variavel automaticamente.
