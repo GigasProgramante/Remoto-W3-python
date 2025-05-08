cab76d1b15dacf0c83e960c4e695dd9c8a3a27cb
Guigas <guilhermevoj@gmail.com>
2025-05-08 15:00:02 -0300

    modificação

--- Exercícios arrays/Extra, Média dinãmica.py
+++ Exercícios arrays/Extra, Média dinãmica.py
@@ -1,15 +1,17 @@
-nums = []  # Inicializa uma lista vazia
+nums = []
 inserir = True
 
 while inserir:
     nota = input("Insira suas notas (deixe vazio para calcular a média): ")
-    if nota == "":  # Verifica se o input está vazio
+    if nota == "":
         inserir = False
     else:
-        nums.append(float(nota))  # Adiciona a nota convertida para float na lista
+        nums.append(float(nota))
 
 if len(nums) > 0:
     media = sum(nums) / len(nums)
     print(f"A média final é de: {media}")
 else:
     print("Nenhuma nota foi inserida.")
+
+# array.append(tipo_de_dado(variável)), adiciona o valor do variável ao final da array, esse é o jeito correto de adicionar dados a uma array vazia, de tamanho indefinido.
