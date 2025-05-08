58f3c177e3414b8dbf612c0c6a752a78c2ca85dc
Guigas <guilhermevoj@gmail.com>
2025-05-08 14:54:21 -0300

    update

---
+++ Exercícios arrays/Extra, Média dinãmica.py
@@ -0,0 +1,16 @@
+nums = []  # Inicializa uma lista vazia
+inserir = True
+
+while inserir:
+    nota = input("Insira suas notas (deixe vazio para calcular a média): ")
+    if nota == "":  # Verifica se o input está vazio
+        inserir = False
+    else:
+        nums.append(float(nota))  # Adiciona a nota convertida para float na lista
+
+# Calcula a média se houver elementos na lista
+if len(nums) > 0:
+    media = sum(nums) / len(nums)
+    print(f"A média final é de: {media}")
+else:
+    print("Nenhuma nota foi inserida.")
